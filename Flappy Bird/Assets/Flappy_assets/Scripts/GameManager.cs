0833ed73b000b8c3fcbf26ea7331656f590b9cac
fariskhasyaiful <fariskhasyaiful@gmail.com>
2021-06-06 15:37:00 +0800

    Initial commit

---
+++ Flappy Bird/Assets/Flappy_assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    //references
+    public GameObject gameOverCanvas;
+    public GameObject score;
+    public GameObject getReadyImg;
+    public GameObject pauseBtn;
+
+    public static Vector2 bottomLeft;
+
+    //game states
+    public static bool gameOver;
+    public static bool gameHasStarted;
+    public static bool gameIsPaused;
+
+    private void Awake()
+    {
+        bottomLeft = Camera.main.ScreenToWorldPoint(new Vector2(0,0));
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameOver = false;
+        gameHasStarted = false;
+        gameIsPaused = false;
+    }
+
+    public void GameHasStarted()
+    {
+        gameHasStarted = true;
+        score.SetActive(true);
+        getReadyImg.SetActive(false);
+        pauseBtn.SetActive(true);
+    }
+
+    public void GameOver()
+    {
+        gameOver = true;
+        score.SetActive(false);
+        gameOverCanvas.SetActive(true);
+        pauseBtn.SetActive(false);
+    }
+
+    public void OnOkBtnPressed()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+    }
+
+    public void onMenuBtnPressed()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Flappy Bird/Assets/Flappy_assets/Scripts/Pause.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+    Image img;
+
+    //references
+    public Sprite playSprite;
+    public Sprite pausedSprite;
+
+    private void Start()
+    {
+        img = GetComponent<Image>();
+    }
+
+    public void OnPauseGame()
+    {
+        if(GameManager.gameIsPaused == false)
+        {
+            Time.timeScale = 0;
+            img.sprite = playSprite;
+            GameManager.gameIsPaused = true;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            img.sprite = pausedSprite;
+            GameManager.gameIsPaused = false;
+        }
+    }
+    
+}
